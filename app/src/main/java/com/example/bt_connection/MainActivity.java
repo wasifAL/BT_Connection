d0653847cd93e0d3fc658389d301d2febec85cbc
WASIF AL MAHMUD <wamvmaw@gmail.com>
2020-01-11 04:54:49 +0600

    added view buttons and controller action added to check if device has bluetooth adapter

--- app/src/main/java/com/example/bt_connection/MainActivity.java
+++ app/src/main/java/com/example/bt_connection/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.bt_connection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVER_BT = 1;
+
+
+    TextView mStatusBTv, mPairedTv;
+    ImageView mBIv;
+    Button mOnBtn, mOffBtn, mDiscoverBtn, mPairedBtn;
+
+    BluetoothAdapter mBlueAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBIv = findViewById(R.id.bluetoothIv);
+        mStatusBTv = findViewById(R.id.stausBluetoothTv);
+        mPairedTv = findViewById(R.id.pairedTv);
+        mPairedBtn = findViewById(R.id.pairedDevicesBtn);
+        mOnBtn = findViewById(R.id.onBtn);
+        mOffBtn = findViewById(R.id.offBtn);
+        mDiscoverBtn = findViewById(R.id.discoverableBtn);
+
+
+        mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(mBlueAdapter == null){
+            mStatusBTv.setText("Bluetooth is not supported by the device");
+        }else{
+            //device has bluetooth adapter
+        }
     }
 }
